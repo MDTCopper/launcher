ec678f95603c48623124c5c27e5995e0d969b960
rainfallllll <rainfall0416@gmail.com>
2026-08-31 20:50:21 +0800

    清理

--- lib/util/io/path_selector.dart
+++ lib/util/io/path_selector.dart
@@ -7,7 +7,7 @@ import '../app_paths.dart';
 import 'process_controller.dart';
 
 class PathSelector {
-  ///打开对应路径文件夹（Win32 防重复打开：已有同路径窗口则激活，否则新建）
+  ///打开对应路径文件夹
   static Future<void> openFolder(String path) async {
     final dir = Directory(path);
     if (!(await dir.exists())) {
@@ -71,7 +71,7 @@ class PathSelector {
 
   //todo 安卓端文件定位
   static Future<void> _locateFileOnAndroid(String path) async {
-    // 步骤1：申请存储权限（Android 13+需读取权限）
+    // 申请存储权限（Android 13+需读取权限）
     final bool hasPermission = await _requestAndroidFilePermission();
     if (!hasPermission) {
       throw Exception('安卓端暂时不支持文件定位');
@@ -81,11 +81,10 @@ class PathSelector {
     final String folderPath = file.parent.path;
 
     try {
-      // 方式1：优先使用open_filex尝试直接定位（部分文件管理器支持）
       final result = await OpenFilex.open(path, type: "file");
 
       if (result.type != ResultType.done) {
-        // 方式2：兜底方案：打开文件所在文件夹，提示用户查找
+        //兜底
         await OpenFilex.open(folderPath, type: "folder");
         print("Android文件管理器不支持直接定位，已打开所在文件夹：$folderPath");
       } else {
@@ -99,7 +98,7 @@ class PathSelector {
   }
 
   static Future<bool> _requestAndroidFilePermission() async {
-    //todo 存储权限申请
+    //TODO 存储权限申请
 
     return false;
   }
